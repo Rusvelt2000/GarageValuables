4805f68626a8d0cfa6d02402b906a3c45b9b1a91
Roberto Work <rusvelt.rr@gmail.com>
2024-05-15 09:16:27 +1000

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <h1>This is the new app</h1>
-=======
-      <h1>Hello World!</h1>
->>>>>>> origin/main
+      <h1>This is the new app!</h1>
     </div>
   );
 }
